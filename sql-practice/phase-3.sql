e1bb374999be189a6d2475971b6a3915223c3c8e
Brandon Tasaki <b.tasaki@gmail.com>
2022-08-11 13:35:24 -0700

    finished phase 3

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,5 +1,20 @@
 -- Find Hermione's cats
 -- Your code here
 
+SELECT cats.name FROM cats JOIN cat_owners ON cats.id = cat_owners.cat_id
+JOIN owners ON cat_owners.owner_id = owners.id
+WHERE owners.first_name = 'Hermione';
+
 -- Find All the Toys for Hermione's cats
 -- Your code here
+
+SELECT toys.name FROM toys JOIN cats ON toys.cat_id = cats.id
+JOIN cat_owners ON cats.id = cat_owners.cat_id
+JOIN owners ON cat_owners.owner_id = owners.id
+WHERE owners.first_name = 'Hermione';
+
+-- toys -> cats -> cat_owners -> owners
+
+SELECT toys.name FROM toys JOIN cats ON toys.cat_id = cats.id
+JOIN cat_owners ON cats.id = cat_owners.cat_id
+WHERE cat_owners.owner_id = 7;
